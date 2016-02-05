6ca5025789589599d543d4927187bde973c91957
zaynrahn <zaynrahn@gmail.com>
2016-02-05 19:02:34 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,7 +2,6 @@
 def position_taken?(board, position)
   if (board[position]==' ')||(board[position]=='')||(board[position]==nil)
     false
-
   elsif (board[position]=='X')||(board[position]=='O')
     true
   end
